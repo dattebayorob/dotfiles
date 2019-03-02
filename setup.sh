237272186886eed5422a69158ccce6df0bc6e34b
dattebayoRob <robson.william65@gmail.com>
2019-03-02 22:39:37 +0000

    Script for copy preferences (system and user files)

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for app in $(ls); do
+	if [ -d $app ]; then
+		for sysDir in $(ls $app); do
+			sudo -S cp -rf ${app}/${sysDir}/* /${sysDir}/ <<< $ENV_PASSWORD
+		done
+		if [ -d $app/.config ]; then
+			if [ ! -d ~/.config/$app ]; then
+				mkdir ~/.config/$app
+			fi
+			cp -rf $app/.config/$app/* ~/.config/$app/
+		fi
+	fi
+done
