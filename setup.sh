e31c02018e2dfe4b91862a281c1f24497284eb0a
dattebayorob <robson.william65@gmail.com>
2019-08-24 12:52:12 -0300

    update script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-for app in $(ls); do
-	if [ -d $app ]; then
-		for sysDir in $(ls $app); do
-			sudo -S cp -rf ${app}/${sysDir}/* /${sysDir}/ <<< $ENV_PASSWORD
-		done
-		if [ -d $app/.config ]; then
-			if [ ! -d ~/.config/$app ]; then
-				mkdir ~/.config/$app
+
+copyAppSettings(){
+	for app in $(ls | sed 's/dotfiles//'); do
+		if [ -d $app ]; then
+			for sysDir in $(ls $app); do
+				sudo -S cp -rf ${app}/${sysDir}/* /${sysDir}/ <<< $ENV_PASSWORD
+			done
+			if [ -d $app/.config ]; then
+				if [ ! -d ~/.config/$app ]; then
+					mkdir ~/.config/$app
+				fi
+				cp -rf $app/.config/$app/* ~/.config/$app/
 			fi
-			cp -rf $app/.config/$app/* ~/.config/$app/
 		fi
-	fi
-done
-cp -rf dotfiles/* ~/
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+	done
+}
+
+copyDotFiles(){
+	cp -rf dotfiles/* ~/
+}
+
+installOhMyZsh(){
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"h -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
+
+if [ -z "$ENV_PASSWORD" ]; then
+	echo "No Passowd declared, do you export ENV_PASSWORD?" 
+else
+	copyAppSettings
+	copyDotFiles
+	installOhMyZsh
+fi
